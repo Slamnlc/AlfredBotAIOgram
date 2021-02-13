fa2cbe43a9bc98648891d1dbd557885a1c93e324
Slamnlc <72922561+Slamnlc@users.noreply.github.com>
2021-02-13 12:24:04 +0200

    1=1

--- loader.py
+++ loader.py
@@ -3,7 +3,7 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from classes.sql import crete_connection, create_yaposhka
 from data.config import TOKEN
-
+1==1
 bot = Bot(token=TOKEN, parse_mode=types.ParseMode.HTML)
 storage = MemoryStorage()
 db = crete_connection()
