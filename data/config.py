bc1198958ebc8cf2a567931107bd887f7a20b703
Slamnlc <72922561+Slamnlc@users.noreply.github.com>
2021-02-13 12:57:27 +0200

    1=1

--- data/config.py
+++
@@ -1,9 +0,0 @@
-TOKEN = '1437484062:AAFYAEQCs3T7723uIEX6lQOiJfjK0QhaXNc'
-IMAGE_PATH = '/Users/maksim/CurrencyBot/images/'
-ADMINS = [185200431]
-
-mainDbName = 'alfredBot'
-yaposhkaDbName = 'yaposhka'
-1==1
-dbUserName = 'postgres'
-dbPassword = '26302630Bb'
