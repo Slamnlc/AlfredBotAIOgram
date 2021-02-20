a4d7bfb8f8c5005f8e1449d1ef4e7556e5e25ca7
Slamnlc <72922561+Slamnlc@users.noreply.github.com>
2021-02-21 01:59:42 +0200

    update for AWS

--- scripts/firstStart.sh
+++ scripts/firstStart.sh
@@ -2,10 +2,10 @@ sudo apt-get update
 git clone https://github.com/Slamnlc/AlfredBotAIOgram
 mv /home/ubuntu/config.py /home/ubuntu/AlfredBotAIOgram/data/
 mv /home/ubuntu/backUp.sql /home/ubuntu/AlfredBotAIOgram/
-sed -i 's/127.0.0.1/172.31.11.161/g' /home/ubuntu/AlfredBotAIOgram/data/config.py
+sed -i 's/127.0.0.1/db/g' /home/ubuntu/AlfredBotAIOgram/data/config.py
 # shellcheck disable=SC2164
 cd /home/ubuntu/AlfredBotAIOgram/
 sudo snap install docker
-sudo sudo docker-compose up -d --build
+sudo sudo docker-compose up --detach
 sudo docker-compose exec db psql -U maksim -f /var/lib/postgresql/backUp.sql
 #sudo sudo docker-compose up
