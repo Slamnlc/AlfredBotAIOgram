a708c6604b2410a4ab968f6fc604987eeaf6224c
Slamnlc <72922561+Slamnlc@users.noreply.github.com>
2021-02-20 22:37:59 +0200

    update for AWS

--- scripts/messages/sendMessages.py
+++ scripts/messages/sendMessages.py
@@ -11,8 +11,9 @@ async def job():
         await dp.bot.send_message(admin, 'Ну шо? Я настроил')
 
 
-schedule.every(10).seconds.do(job)
-loop = asyncio.get_event_loop()
-while 1:
-    loop.run_until_complete(schedule.run_pending())
-    time.sleep(1)
+if __name__ == '__main__':
+    schedule.every(10).seconds.do(job)
+    loop = asyncio.get_event_loop()
+    while 1:
+        loop.run_until_complete(schedule.run_pending())
+        time.sleep(1)
