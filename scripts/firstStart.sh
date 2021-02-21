496a7661b116c98bef31c8ed2ce86f75800b0b88
Slamnlc <72922561+Slamnlc@users.noreply.github.com>
2021-02-21 03:32:38 +0200

    update for AWS

--- scripts/firstStart.sh
+++ scripts/firstStart.sh
@@ -6,12 +6,14 @@ mv /home/ubuntu/main.sql /home/ubuntu/AlfredBotAIOgram/
 mv /home/ubuntu/yap.sql /home/ubuntu/AlfredBotAIOgram/
 sed -i 's/127.0.0.1/db/g' /home/ubuntu/AlfredBotAIOgram/data/config.py
 sed -i 's/postgres/maksim/g' /home/ubuntu/AlfredBotAIOgram/data/config.py
+sed -i 's/alfredBot/alfredbot/g' /home/ubuntu/AlfredBotAIOgram/data/config.py
+sed -i 's/1437484062:AAFYAEQCs3T7723uIEX6lQOiJfjK0QhaXNc/1678753173:AAFph0Mx901NE7fCA1BPAoeQDYXIE7e3wiU/g' /home/ubuntu/AlfredBotAIOgram/data/config.py
 # shellcheck disable=SC2164
 cd /home/ubuntu/AlfredBotAIOgram/
 sudo snap install docker
 sudo sudo docker-compose up --detach --build
-sudo docker-compose exec -T db psql -c "create database alfredBot"
+sudo docker-compose exec -T db psql -c "create database alfredbot"
 sudo docker-compose exec -T db psql -c "create database yaposhka"
-sudo docker-compose exec -T db psql -U maksim alfredBot /var/lib/postgresql/main.sql
-sudo docker-compose exec -T db psql -U maksim yaposhka /var/lib/postgresql/yap.sql
+sudo docker-compose exec -T db psql -U maksim alfredbot -f /var/lib/postgresql/main.sql
+sudo docker-compose exec -T db psql -U maksim yaposhka -f /var/lib/postgresql/yap.sql
 sudo sudo docker-compose up
