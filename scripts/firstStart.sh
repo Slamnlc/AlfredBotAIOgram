3372bc48dae9f3061bc02666a01172de93834836
Slamnlc <72922561+Slamnlc@users.noreply.github.com>
2021-02-21 02:41:14 +0200

    update for AWS

--- scripts/firstStart.sh
+++ scripts/firstStart.sh
@@ -1,12 +1,16 @@
 sudo apt-get update
 git clone https://github.com/Slamnlc/AlfredBotAIOgram
 mv /home/ubuntu/config.py /home/ubuntu/AlfredBotAIOgram/data/
-mv /home/ubuntu/backUp.sql /home/ubuntu/AlfredBotAIOgram/
+mv /home/ubuntu/main.sql /home/ubuntu/AlfredBotAIOgram/
+mv /home/ubuntu/yap.sql /home/ubuntu/AlfredBotAIOgram/
 sed -i 's/127.0.0.1/db/g' /home/ubuntu/AlfredBotAIOgram/data/config.py
+sed -i 's/postgres/maksim/g' /home/ubuntu/AlfredBotAIOgram/data/config.py
 # shellcheck disable=SC2164
 cd /home/ubuntu/AlfredBotAIOgram/
 sudo snap install docker
-sudo sudo docker-compose up --detach
-sudo docker-compose exec db psql -U maksim -f /var/lib/postgresql/main.sql
-sudo docker-compose exec db psql -U maksim -f /var/lib/postgresql/yap.sql
+sudo sudo docker-compose up --detach --build
+sudo docker-compose exec db psql -c "create database alfredBot"
+sudo docker-compose exec db psql -c "create database yaposhka"
+sudo docker-compose exec db psql -U maksim alfredBot /var/lib/postgresql/main.sql
+sudo docker-compose exec db psql -U maksim yaposhka /var/lib/postgresql/yap.sql
 sudo sudo docker-compose up
