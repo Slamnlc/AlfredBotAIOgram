1d6cb29a2d8fbc7881a874b9e41787c33fcbcbad
Slamnlc <72922561+Slamnlc@users.noreply.github.com>
2021-02-23 20:14:37 +0200

    update for AWS

--- scripts/firstStart.sh
+++ scripts/firstStart.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 sudo apt-get update
-#git clone https://github.com/Slamnlc/AlfredBotAIOgram
+git clone https://github.com/Slamnlc/AlfredBotAIOgram
 mv /home/ubuntu/config.py /home/ubuntu/AlfredBotAIOgram/data/
 mv /home/ubuntu/main.sql /home/ubuntu/AlfredBotAIOgram/
+sudo mv /home/ubuntu/yap.sql /home/ubuntu/AlfredBotAIOgram/
+sudo mv /home/ubuntu/alfred.sql /home/ubuntu/AlfredBotAIOgram/
 sed -i 's/127.0.0.1/db/g' /home/ubuntu/AlfredBotAIOgram/data/config.py
-sed -i 's//Users/maksim/CurrencyBot/images/images/g' /home/ubuntu/AlfredBotAIOgram/data/config.py
+sed -i 's/\/Users\/maksim\/CurrencyBot\/images/\ images/g' /home/ubuntu/AlfredBotAIOgram/data/config.py
 #sed -i 's/alfredBot/alfredbot/g' /home/ubuntu/AlfredBotAIOgram/data/config.py
 sed -i 's/1437484062:AAFYAEQCs3T7723uIEX6lQOiJfjK0QhaXNc/1678753173:AAFph0Mx901NE7fCA1BPAoeQDYXIE7e3wiU/g' /home/ubuntu/AlfredBotAIOgram/data/config.py
 # shellcheck disable=SC2164
 cd /home/ubuntu/AlfredBotAIOgram/
 sudo snap install docker
 sudo sudo docker-compose up --detach --build
-sudo docker-compose exec -T db psql -U postgres -f /var/lib/postgresql/main.sql
-sudo sudo docker-compose up
+sudo docker-compose exec -T db psql -U postgres -f -C /var/lib/postgresql/alfred.sql
+sudo docker-compose exec -T db psql -U postgres -f -C /var/lib/postgresql/yap.sql
+#sudo docker-compose exec -T db psql -U postgres -f /var/lib/postgresql/main.sql
+#sudo sudo docker-compose up
