0851c733495798fdadbbc0eedc00f1e5070784c0
Slamnlc <72922561+Slamnlc@users.noreply.github.com>
2021-02-23 20:59:40 +0200

    update for AWS

--- scripts/firstStart.sh
+++ scripts/firstStart.sh
@@ -13,7 +13,9 @@ sed -i 's/1437484062:AAFYAEQCs3T7723uIEX6lQOiJfjK0QhaXNc/1678753173:AAFph0Mx901N
 cd /home/ubuntu/AlfredBotAIOgram/
 sudo snap install docker
 sudo sudo docker-compose up --detach --build
-sudo docker-compose exec -T db psql -U postgres -f -C /var/lib/postgresql/alfred.sql
-sudo docker-compose exec -T db psql -U postgres -f -C /var/lib/postgresql/yap.sql
+sudo docker-compose exec -T db psql -U postgres -c 'create database alfredbot'
+sudo docker-compose exec -T db psql -U postgres -c 'create database yaposhka'
+sudo docker-compose exec -T db pg_dump -U postgres -C alfredbot < alfred.sql
+sudo docker-compose exec -T db pg_dump -U postgres -C yaposhka < yap.sql
 #sudo docker-compose exec -T db psql -U postgres -f /var/lib/postgresql/main.sql
 #sudo sudo docker-compose up
