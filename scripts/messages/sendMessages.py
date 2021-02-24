f9c2074c12c05b5a496cb1ab6809986ec3e463c2
Slamnlc <72922561+Slamnlc@users.noreply.github.com>
2021-02-24 22:55:29 +0200

    update for AWS

--- scripts/messages/sendMessages.py
+++ scripts/messages/sendMessages.py
@@ -16,11 +16,10 @@ async def job():
 
 
 async def startSchedule():
-    schedule.every(30).seconds.do(job)
+    # schedule.every(30).seconds.do(job)
     schedule.every().day.at("18:00").do(addCurrencyInfo, 5)
     schedule.every().day.at("22:00").do(addCurrencyInfo, 5)
     schedule.every().day.at("06:00").do(addCurrencyInfo, 5)
-    schedule.every().day.at("20:53").do(addCurrencyInfo, 5)
     while True:
         await schedule.run_pending()
         await asyncio.sleep(1)
