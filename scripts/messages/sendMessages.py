01473fdf6d67372280b3ab6cc696a940dc0547cc
Slamnlc <72922561+Slamnlc@users.noreply.github.com>
2021-02-24 22:50:25 +0200

    update for AWS

--- scripts/messages/sendMessages.py
+++ scripts/messages/sendMessages.py
@@ -1,4 +1,5 @@
 import logging
+from datetime import datetime
 
 import aioschedule as schedule
 import asyncio
@@ -8,13 +9,14 @@ from service.functions.currency_function import addCurrencyInfo
 
 
 async def job():
+    now = datetime.now()
     for admin in ADMINS:
-        await dp.bot.send_message(admin, 'Ну шо? Я настроил')
+        await dp.bot.send_message(admin, now.time())
     logging.info('Nu sho?')
 
 
 async def startSchedule():
-    schedule.every(30).seconds.do(job)
+    schedule.every(10).seconds.do(job)
     schedule.every().day.at("18:00").do(addCurrencyInfo, 5)
     schedule.every().day.at("22:00").do(addCurrencyInfo, 5)
     schedule.every().day.at("06:00").do(addCurrencyInfo, 5)
