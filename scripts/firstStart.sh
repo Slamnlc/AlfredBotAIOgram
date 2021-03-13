d07080f74353546fc160a6cae34c4d31cfe58aa1
Slamnlc <72922561+Slamnlc@users.noreply.github.com>
2021-03-13 13:54:42 +0200

    notification update

--- scripts/firstStart.sh
+++ scripts/firstStart.sh
@@ -3,19 +3,19 @@ sudo timedatectl set-timezone Europe/Kiev
 sudo apt-get update
 git clone https://github.com/Slamnlc/AlfredBotAIOgram
 
-mv /home/ubuntu/config.py /home/ubuntu/AlfredBotAIOgram/data/
-sudo mv /home/ubuntu/backup.sql /home/ubuntu/AlfredBotAIOgram/
-sudo mv /home/ubuntu/.env /home/ubuntu/AlfredBotAIOgram/
-sudo mv /home/ubuntu/makeBackUp.sh /home/ubuntu/AlfredBotAIOgram/scripts/
-sudo chmod 0644 /home/ubuntu/AlfredBotAIOgram/scripts/makeBackUp.sh
+mv /home/config.py /home/AlfredBotAIOgram/data/
+sudo mv /home/backup.sql /home/AlfredBotAIOgram/
+sudo mv /home/.env /home/AlfredBotAIOgram/
+sudo mv /home/makeBackUp.sh /home/AlfredBotAIOgram/scripts/
+sudo chmod 0644 /home/AlfredBotAIOgram/scripts/makeBackUp.sh
 
-sed -i 's/127.0.0.1/db/g' /home/ubuntu/AlfredBotAIOgram/data/config.py
-sed -i 's/\/Users\/maksim\/CurrencyBot\/images/\/images/g' /home/ubuntu/AlfredBotAIOgram/data/config.py
-sed -i 's/alfredBot/alfredbot/g' /home/ubuntu/AlfredBotAIOgram/data/config.py
-sed -i 's/1437484062:AAFYAEQCs3T7723uIEX6lQOiJfjK0QhaXNc/1678753173:AAFph0Mx901NE7fCA1BPAoeQDYXIE7e3wiU/g' /home/ubuntu/AlfredBotAIOgram/data/config.py
+sed -i 's/127.0.0.1/db/g' /home/AlfredBotAIOgram/data/config.py
+sed -i 's/\/Users\/maksim\/CurrencyBot\/images/\/images/g' /home/AlfredBotAIOgram/data/config.py
+sed -i 's/alfredBot/alfredbot/g' /home/AlfredBotAIOgram/data/config.py
+sed -i 's/1437484062:AAFYAEQCs3T7723uIEX6lQOiJfjK0QhaXNc/1678753173:AAFph0Mx901NE7fCA1BPAoeQDYXIE7e3wiU/g' /home/AlfredBotAIOgram/data/config.py
 
 # shellcheck disable=SC2164
-cd /home/ubuntu/AlfredBotAIOgram/
+cd /home/AlfredBotAIOgram/
 sudo snap install docker
 sudo sudo docker-compose up --detach --build
 sudo docker-compose exec -T db psql -U postgres  -f /var/lib/postgresql/backup.sql
